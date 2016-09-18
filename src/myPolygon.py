5fa9facaa920ca81a8254a8bdf33b79b61a6e0b0
Teng Li <manutd062414@gmail.com>
2016-09-18 14:56:57 +0800

    update

--- src/myPolygon.py
+++ src/myPolygon.py
@@ -5,17 +5,28 @@ Created on Sep 18, 2016
 '''
 
 from swampy.TurtleWorld import *
+from math import *
+
+def square(t, length):
+    for i in range(4):
+        fd(t, length)
+        lt(t)
+
+def polygon(t, n, length):
+    for i in range(n):
+        fd(t, length)
+        lt(t, 360.0 / n)
+
+def circle(t, r):
+    length = 2*pi*r/360
+    polygon(t, 360, length)
 
 world = TurtleWorld()
 bob = Turtle()
+bob.pen_color='black';
 print bob
+bob.delay=0.01
 
-fd(bob, 100)
-lt(bob)
-fd(bob, 100)
-lt(bob)
-fd(bob, 100)
-lt(bob)
-fd(bob, 100)
+circle(bob, 100)
 
 wait_for_user()
