497679df9b1e3c28a3e722c054e484fb3965aec6
Teng Li <manutd062414@gmail.com>
2016-09-18 14:26:31 +0800

    add polygons

---
+++ src/myPolygon.py
@@ -0,0 +1,21 @@
+'''
+Created on Sep 18, 2016
+
+@author: omega
+'''
+
+from swampy.TurtleWorld import *
+
+world = TurtleWorld()
+bob = Turtle()
+print bob
+
+fd(bob, 100)
+lt(bob)
+fd(bob, 100)
+lt(bob)
+fd(bob, 100)
+lt(bob)
+fd(bob, 100)
+
+wait_for_user()
